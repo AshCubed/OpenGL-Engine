f9288bf60bfce2dafb5b938fbd1a4e4ef17ee4b6
AshCubed <43545267+AshCubed@users.noreply.github.com>
2020-10-14 00:59:00 +0200

    ASSIMP Included

--- OpenGLDemo/OpenGLDemo/OpenGLDemo.cpp
+++ OpenGLDemo/OpenGLDemo/OpenGLDemo.cpp
@@ -1,11 +1,14 @@
 #include"Game.h"
-#include"FileReader.h"
 using namespace std;
+
+////Tutorial Used
+//Sharma, S. (2020). C++ OpenGL 3D Tutorial - 
+//YouTube. [online] www.youtube.com. 
+//Available at: https://www.youtube.com/playlist?list=PL6xSOsbVA1eYSZTKBxnoXYboy7wc4yg-Z [Accessed 29 Sep. 2020].
 
 
 int main(void)
 {
-    
     Game game("Hi Ashveer", 640, 480, 3, 3, true);
 
     //string temp;
